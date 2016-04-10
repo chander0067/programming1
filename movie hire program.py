aa3abc13d44eeff25c9af4a19fbb28ead66d0fbd
chander0067 <chanderdeep@my.jcu.edu.au>
2016-04-10 22:43:35 +1000

    hire items function has been created

--- movie hire program.py
+++ movie hire program.py
@@ -1,3 +1,90 @@
+""" hireItems Function
+function hireItems():
+oprn file ("items.csv",'r')
+read file f.readlines()
+     set n=0
+    hList=[]
+for i in item:
+        movieName = i.strip().split(',')[0]
+        description = i.strip().split(',')[1]
+        cost = float(i.strip().split(',')[2])
+        availiblity = i.strip().split(',')[-1]
+
+if availablity == "in":
+            say (str(n)+" - "+"%-45s"%(movieName+"("+description+")")+"= $"+"%7.2f"%(cost),sep=" ")
+            n+=1
+            hList.append(i)
+
+close file f.close()
+while True:
+movieChoice = int(input("Enter the number of an item to hire "))
+if movieChoice >= 0 and movieChoice < n:
+
+           open file in write mode f= open("items.csv",'w')
+
+            m = item.index(hList[movieChoice])
+
+
+            movie = item[m]
+            movieName = movie.strip().split(',')[0]
+            description = movie.strip().split(',')[1]
+            cost = float(movie.strip().split(',')[2])
+            print (movieName+ " hired for $"+ "%.2f" %(cost) )
+            item[m] = movieName+","+description+","+str(cost)+","+"out\n"
+            file=""
+            for i in item:
+            add a file after each execution    file = file + i
+             write file say f.write(file)
+            f close file say .close()
+            break
+
+        else:
+            say ("Invalid input. \n")
+
+"""
+def hireItems():
+    f= open("items.csv",'r')
+    item= f.readlines()
+    n=0
+    hList=[]
+    for i in item:
+        movieName = i.strip().split(',')[0]
+        description = i.strip().split(',')[1]
+        cost = float(i.strip().split(',')[2])
+        availiblity = i.strip().split(',')[-1]
+
+        if availiblity == "in":
+            print(str(n)+" - "+"%-45s"%(movieName+"("+description+")")+"= $"+"%7.2f"%(cost),sep=" ")
+            n+=1
+            hList.append(i)
+
+    f.close()
+    while True:
+        movieChoice = int(input("Enter the number of an item to hire "))
+        if movieChoice >= 0 and movieChoice < n:
+
+            f= open("items.csv",'w')
+
+            m = item.index(hList[movieChoice])
+
+
+            movie = item[m]
+            movieName = movie.strip().split(',')[0]
+            description = movie.strip().split(',')[1]
+            cost = float(movie.strip().split(',')[2])
+            print (movieName+ " hired for $"+ "%.2f" %(cost) )
+            item[m] = movieName+","+description+","+str(cost)+","+"out\n"
+            file=""
+            for i in item:
+                file = file + i
+            f.write(file)
+            f.close()
+            break
+
+        else:
+            print("Invalid input. \n")
+
+
 """LIST of MOVIES
 # function listMovies() then say”All items on file(* indicates item is currently out)”
 open file ("items.csv",'r')
@@ -20,7 +107,7 @@ open file ("items.csv",'r')
 """
 def listMovies():
     print("All items on file (* indicates item is currently out):")
-f= open("items.csv",'r')
+    f= open("items.csv",'r')
     movie= f.readlines()
     n=0
     for i in movie:
